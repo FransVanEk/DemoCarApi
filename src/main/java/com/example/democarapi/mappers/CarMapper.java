487a288bf0c3f9a1dcef8c1ed41f5c3c6dfc5493
annaezer <annaezer@gmail.com>
2023-09-26 11:49:23 +0200

    Added capitalizer as helper function for brand and model

--- src/main/java/com/example/democarapi/mappers/CarMapper.java
+++ src/main/java/com/example/democarapi/mappers/CarMapper.java
@@ -1,15 +1,19 @@
 package com.example.democarapi.mappers;
 
 import com.example.democarapi.entities.Car;
+import com.example.democarapi.helpers.Capitalize;
 import com.example.democarapi.models.cars.CarResponse;
 
 public class CarMapper {
     public static CarResponse ToResponse(Car entity)
     {
+        String CapitalizedBrand = Capitalize.capitalizeWord(entity.getBrand());
+        String CapitalizedModel = Capitalize.capitalizeWord(entity.getModel());
+
         return new CarResponse(
                 entity.getId(),
-                entity.getBrand(),
-                entity.getModel(),
+                CapitalizedBrand,
+                CapitalizedModel,
                 entity.getVin()
         );
     }
